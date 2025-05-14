7a809c10a83269cbb7d24042a76fa51db5e827ce
futig <everymusic8@gmail.com>
2025-05-14 23:38:42 +0500

    lobby fix

--- public/js/controllers/lobby-controller/lobby-controller.js
+++ public/js/controllers/lobby-controller/lobby-controller.js
@@ -1,5 +1,4 @@
 import socket from '../socket.js';
-import { initSocketEvents } from './events.js';
 import { renderMainMenu } from '../../utils/router.js';
 import { sendChatMessage } from '../chat/chat-controller.js';
 
